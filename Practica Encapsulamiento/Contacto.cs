d6816fa911bbe3bc16667dc89d0ead9b20955c5d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 08:45:55 -0700

    Agregar archivos de proyecto.

---
+++ Practica Encapsulamiento/Contacto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Encapsulamiento
+{
+    class Contacto
+    {
+        //Auto propeties - No punto y coma al final. 
+        public string Nombre{ get; set; }
+        public string Direccion { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        //Propierties
+        private string _telefonoCelular;
+        public string TelefonoCelular
+        {
+            get { return _telefonoCelular; }
+            set
+            {
+                if (value.Length == 10)
+                {
+                    _telefonoCelular = value;
+                }
+                else if (value.Length == 7)
+                {
+                    _telefonoCelular = "644" + value;
+                }
+                else
+                {
+                    Console.WriteLine("Introduce un numero de celular valido");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Practica Encapsulamiento/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Practica_Encapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contacto contactoUno = new Contacto();
+
+            contactoUno.Nombre = "José García";
+            contactoUno.Direccion = "Veracruz 510";
+            contactoUno.CorreoElectronico = "jgarcia@gmail.com";
+            contactoUno.TelefonoCasa = "4131605";
+            contactoUno.TelefonoCelular = "1039";
+
+            Console.WriteLine("Nombre: " + contactoUno.Nombre);
+            Console.WriteLine("Dirección: " + contactoUno.Direccion);
+            Console.WriteLine("Correo: " + contactoUno.CorreoElectronico);
+            Console.WriteLine("Telefono: " + contactoUno.TelefonoCasa);
+            Console.WriteLine("Celular: " + contactoUno.TelefonoCelular);
+
+            Console.ReadLine();
+        }
+
+    }
+}
